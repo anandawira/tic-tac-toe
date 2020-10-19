b758ad901b4017a4cc191e81b3dda80fbf9b9fbb
Ananda Wiradharma <anandawira754@gmail.com>
2020-10-19 16:11:02 +0700

    add comments, change some styling

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// Gameboard Module.
 const gameBoard = (() => {
+  // Initialize empty board array.
   let board = Array(9).fill("");
+
   const renderBoard = () => {
+    // Change cell's innertext based on board array value.
     board.forEach((symbol, index) => {
       document.getElementById(index).innerText = symbol;
     });
@@ -8,21 +12,21 @@ const gameBoard = (() => {
 
   const markSymbol = (cell, symbol) => {
     if (board[cell] == "") {
-      board[cell] = symbol;
+      // Check if current cell is blank before continue the process.
+      board[cell] = symbol; // Assign symbol to board array.
       renderBoard();
       return true;
     } else {
-      console.log("Cell is already marked");
       return false;
     }
   };
 
   const checkTie = () => {
-    const cells = Array.from(document.getElementsByClassName("cell"));
+    const cells = Array.from(document.getElementsByClassName("cell")); // get all cell element.
     let tie = true;
     for (let cell of cells) {
       if (cell.innerText == "") {
-        tie = false;
+        tie = false; // If there is at least one element '', then return false.
         break;
       }
     }
@@ -38,30 +42,32 @@ const gameBoard = (() => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+  ]; // Manual written winning combination.
 
   const checkWinner = () => {
-    winner = undefined;
+    let winner = undefined;
     winningCombinations.forEach((combination) => {
       const a = board[combination[0]];
       const b = board[combination[1]];
       const c = board[combination[2]];
       if (a == b && a == c && a != "") {
+        // check if 3 cell of winning combination has same value and not ''.
         winner = a;
       }
     });
     if (checkTie()) {
+      // check if tie.
       winner = "tie";
     }
     return winner;
   };
 
   const resetBoard = () => {
-    board = Array(9).fill("");
+    board = Array(9).fill(""); // reset board to empty array.
     renderBoard();
   };
 
-  return { renderBoard, markSymbol, checkWinner, resetBoard, checkTie };
+  return { markSymbol, checkWinner, resetBoard, checkTie };
 })();
 
 const Player = (name, symbol) => {
@@ -90,45 +96,62 @@ const displayController = (() => {
   const markCell = (cell) => {
     const success = gameBoard.markSymbol(cell, getCurrentPlayer().getSymbol());
     if (success) {
-      switchPlayer();
+      switchPlayer(); // change turn.
       if (gameBoard.checkWinner()) {
+        // check winner.
         let winText = "";
+
         if (gameBoard.checkWinner() == "X") {
           winText = `${player1.getName()}<br>WIN !!!`;
         } else if (gameBoard.checkWinner() == "O") {
           winText = `${player2.getName()}<br>WIN !!!`;
         } else if (gameBoard.checkWinner() == "tie") {
           winText = `TIE !!!`;
-        }
+        } // make text to show based on the checkWinner function response.
 
-        document.getElementById("game").classList.add("hidden");
+        document.getElementById("game").classList.add("hidden"); //Hide game element.
+
+        // Show menu and winner display element.
         with (document.getElementById("winnerDisplay")) {
           innerHTML = winText;
           classList.remove("hidden");
         }
         document.getElementById("menu").classList.remove("hidden");
+        //
       }
     }
   };
 
   const startGame = () => {
+    // get input elements
     const input1 = document.getElementById("txtPlayer1");
     const input2 = document.getElementById("txtPlayer2");
+    //
 
+    // get value from input, if null the take the placeholder.
     const name1 = input1.value || input1.getAttribute("placeholder");
     const name2 = input2.value || input2.getAttribute("placeholder");
+    //
 
+    // assign player1 and player2 with new instance of function factory Player with new name.
     player1 = Player(name1.toUpperCase(), "X");
     player2 = Player(name2.toUpperCase(), "O");
-    gameBoard.resetBoard();
-    currentPlayerIndex = true;
+    //
+
+    gameBoard.resetBoard(); // reset board
+    currentPlayerIndex = true; // resert turn
+
+    // hide winner display and menu element.
+    document.getElementById("winnerDisplay").classList.add("hidden");
     document.getElementById("menu").classList.add("hidden");
-    document.getElementById("game").classList.remove("hidden");
-    console.log("Game Started");
+    //
+
+    document.getElementById("game").classList.remove("hidden"); // show game element
   };
-  return { markCell, startGame };
+  return { startGame, markCell };
 })();
 
+// Assign onclick event to each cell to call function with cell's id as parameter.
 with (Array.from(document.getElementsByClassName("cell"))) {
   forEach((cell) => {
     const id = cell.getAttribute("id");
@@ -137,3 +160,4 @@ with (Array.from(document.getElementsByClassName("cell"))) {
     });
   });
 }
+//
